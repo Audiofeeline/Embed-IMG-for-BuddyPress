b47d55ef86e7f24972e2316adfb8eb99ee71df8c
Audiofeeline <contact@audiofeeline.net>
2017-08-05 09:12:57 +0200

    Create bp-embed-img.php

---
+++ bp-embed-img.php
@@ -0,0 +1,26 @@
+<?php
+add_filter('bp_get_activity_content_body','bp_embed_img', 9);
+add_filter('bp_get_activity_content','bp_embed_img', 9);
+add_filter('bp_get_the_topic_post_content','bp_embed_img', 9);
+function bp_embed_img($content) {
+
+
+if (stripos($content, 'https') !== FALSE) {
+
+ 	$pattern = '!https://([a-z0-9\-\.\/\_]+\.(?:jpe?g|png|gif))!Ui';
+    $replace='<img src="https://$1">';    
+    return preg_replace($pattern,$replace,$content);
+
+}
+
+else {
+
+ 	$pattern = '!http://([a-z0-9\-\.\/\_]+\.(?:jpe?g|png|gif))!Ui';
+    $replace='<img src="http://$1">';    
+    return preg_replace($pattern,$replace,$content);
+
+}
+		
+
+}
+?>
